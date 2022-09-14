0fd17a761f6497243cadef8fb3085b1ff4cc0a19
Pradhyuman-sharma <sharmapuru0642@gmail.com>
2022-09-15 02:04:09 +0530

    adding ui formula

---
+++ keploy-ui.rb
@@ -0,0 +1,17 @@
+# Documentation: https://docs.brew.sh/Formula-Cookbook
+#                https://rubydoc.brew.sh/Formula
+# PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
+class KeployUi < Formula
+  desc "UI for Keploy platform that does API testing by dependency mocking without writing any test cases."
+  homepage "https://keploy.io"
+  url "https://github.com/Pradhyuman-sharma/keploy-ui/archive/refs/tags/v1.0.0.tar.gz"
+  sha256 "2fa94446bd57669d506be507e64b9f6fc6ad444d2f0213fab2c3e8e02309c2c7"
+  license "Apache-2.0"
+
+  depends_on "gatsby" => :build
+  def install
+      system "npm","install"
+      system "gatsby","build"
+      bin.install "keployui"
+  end
+end
